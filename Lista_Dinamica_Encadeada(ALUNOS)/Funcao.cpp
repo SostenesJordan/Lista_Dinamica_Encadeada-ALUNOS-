a7f94525423878ecddc063ba0d0a226894d29994
Sóstenes Jordan <sostenesj60@gmail.com>
2021-04-27 19:22:07 -0300

    Versao comentada

--- Lista_Dinamica_Encadeada(ALUNOS)/Funcao.cpp
+++ Lista_Dinamica_Encadeada(ALUNOS)/Funcao.cpp
@@ -13,20 +13,28 @@ struct elemento {
 
 typedef struct elemento Elem;
 
+// Função reserva um espaço na memoria
 Lista* cria_lista() {
-    Lista* li = (Lista*)malloc(sizeof(Lista));
-
+    Lista* li = (Lista*)malloc(sizeof(Lista)); 
     if (li != NULL) {
         *li = NULL;
     }
     return li;
 }
 
+
+
+
+
+//                  Liberar lista
+// A função irá percorrer todos os elementos cadastrados na lista
+// Andando em cada posição até a sua ultima posição preenchida...
+// Quando chegar na ultima ela será feito o free(lista) >> LIMPAR LISTA
 void libera_lista(Lista* li) {
 
     if (li == NULL) {
         Elem* no;
-        while (*li != NULL)
+        while (*li != NULL)//
         {
             no = *li;
             *li = (*li)->prox;
@@ -35,6 +43,15 @@ void libera_lista(Lista* li) {
         free(li);
     }
 }
+// FIM LIBERA LISTA
+
+
+//------------------------------------------------------------------------------------------
+
+
+// Primeiramente a função irá verificar se a lista tem algum elemento
+// Depois com a variavel no que vai receber um valor por vez da lista por meio do while
+// E o contador vai receber (cont) a quantidade de loops que foram feitos (cada loop representa um aluno cadastrado)
 
 int tamanho_lista(Lista* li) {
 
@@ -61,7 +78,14 @@ int tamanho_lista(Lista* li) {
     return cont;
     cout <<" "<< cont;
 }
+// FIM TAMANHO DA LISTA
 
+
+//------------------------------------------------------------------------------------------
+
+
+// Basicamente é a mesma verificação que são feitas no incio de todas as funções...
+// Se lista retornar NULL irá retornar a mesagem que não tem elementos.
 int lista_vazia(Lista* li) {
 
     if (li == NULL || *li == NULL) {
@@ -69,7 +93,17 @@ int lista_vazia(Lista* li) {
     }
     return 0;
 }
+// FIM LISTA VAZIA
 
+
+//------------------------------------------------------------------------------------------
+
+
+
+// 1° Reserva um novo espaço para o novo aluno
+// ai 'no' (Aluno) vai apontar o elemento anterior para a proxima posição
+// e esse posição vazia vai receber o novo aluno
+// Todos os dados serão recebidos no arquivo principal.
 int insere_aluno_ordenada_matricula(Lista* li, struct aluno al) {
 
     if (li == NULL) {
@@ -108,7 +142,17 @@ int insere_aluno_ordenada_matricula(Lista* li, struct aluno al) {
         return 1;
     }
 }
+// FIM INSERE ALUNO
 
+
+
+//------------------------------------------------------------------------------------------
+
+
+
+// Basicamente é a mesma lógica da função que diz a quantidade de elementos da lista
+// A diferença é que 'no' (alunos) vai apontar para os dados, e dados irá apontar para matricula e nome.
+// Isso tudo dentro de um loop que irá percorrer cada aluno e pegar os seus dados imprimindo para o usuario o resultado.
 void imprime_lista(Lista* li) {
 
     if (li == NULL) {
@@ -123,7 +167,19 @@ void imprime_lista(Lista* li) {
         no = no->prox;
     }
 }
+//FIM IMPRIMI LISTA
 
+
+
+//------------------------------------------------------------------------------------------
+
+
+// Ela é bem parecida com a de adicionar
+// Enquanto o ponteiro move um alemento para frente em forma de fila crescente
+// A função de remover funciona como uma fila decrescente.
+// Lembrando que a remoção é feito pelo n° da matricula, então ele vai buscar o valor referente da matricula informada
+// ai caso não tenha niguem com a matricula informada ele irá exibir uma mensagem
+// ocorrendo tudo certo ele vai da um Free (LIMPAR) no elemento buscado
 int remove_lista(Lista* li, int mat) {
 
     if (li == NULL) {
@@ -151,7 +207,16 @@ int remove_lista(Lista* li, int mat) {
 
     return 1;
 }
+// FIM REMOVE LISTA
 
+
+//------------------------------------------------------------------------------------------
+
+
+// 1° ele recebe vai receber um valor de matricula
+// e com esse valor ele vai fazer uma busca para ver se o aluno existe
+// Ai caso o aluno exista todos os irão para o 'no' e serão apontados para os seus dados
+// feito isso ele vai perguntar se quer adicionar nota do aluno, pq o seus status anterior é de 'cursando...' pois não tem notas ainda
 int consulta_lista_mat(Lista* li, int mat, struct aluno* al) {
 
     if (li == NULL) {
@@ -196,6 +261,10 @@ int consulta_lista_mat(Lista* li, int mat, struct aluno* al) {
 
     }
 }
+// FIM CONSULTA LISTA
+
+
+//------------------------------------------------------------------------------------------
 
 void liberarLista(Lista* li) {
     free(li);
